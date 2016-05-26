8d135d32914b105b41ac6a130273300da77a66ec
pangsir8983 <pangsir8983@gmail.com>
2016-05-26 20:00:27 +0800

    Java方面的一些简单的正则表达式,对于正则有待练习

---
+++ src/main/java/com/pangsir/helper/regex/RegexHelper.java
@@ -0,0 +1,115 @@
+package com.pangsir.helper.regex;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author 刘文铭
+ * @日期 2012 2012-12-20 上午10:33:27
+ * @描述
+ */
+/**
+ * 预先定义好的服务单要使用的正则表达式
+ *
+ * @author 刘文铭
+ * @see com.pangsir.helper.regex.RegexHelper
+ * @since 1.0
+ */
+public class RegexHelper {
+    /**
+     * 正则表达式Pattern： chinese
+     */
+    public static final Pattern pattern_chinese = Pattern.compile("^[\\u4E00-\\u9FA5\\uF900-\\uFA2D]+$", Pattern.CASE_INSENSITIVE);
+    /**
+     * 正则表达式Pattern： ascii
+     */
+    public static final Pattern pattern_ascii = Pattern.compile("^[\\x00-\\xFF]+$", Pattern.CASE_INSENSITIVE);
+    /**
+     * 正则表达式Pattern： Email
+     */
+    public static final Pattern pattern_email = Pattern.compile("^((([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+(\\.([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(\\\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.?$", Pattern.CASE_INSENSITIVE);
+    /**
+     * 正则表达式Pattern： Url
+     */
+    public static final Pattern pattern_url = Pattern.compile("^(https?|ftp):\\/\\/(((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:)*@)?(((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]))|((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.?)(:\\d*)?)(\\/((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)+(\\/(([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)*)*)?)?(\\?((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)|[\\uE000-\\uF8FF]|\\/|\\?)*)?(\\#((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)|\\/|\\?)*)?$", Pattern.CASE_INSENSITIVE);
+    /**
+     * 正则表达式Pattern： Email2
+     */
+    public static final Pattern pattern_email2 = Pattern.compile("^((([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+(\\.([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(\\\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)*(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.?$", Pattern.CASE_INSENSITIVE);
+    /**
+     * 正则表达式Pattern： Url2
+     */
+    public static final Pattern pattern_url2 = Pattern.compile("^(https?|ftp):\\/\\/(((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:)*@)?(((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]))|((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)*(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.?)(:\\d*)?)(\\/((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)+(\\/(([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)*)*)?)?(\\?((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)|[\\uE000-\\uF8FF]|\\/|\\?)*)?(\\#((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)|\\/|\\?)*)?$", Pattern.CASE_INSENSITIVE);
+    /**
+     * 正则表达式Pattern： date-iso
+     */
+    public static final Pattern pattern_date_ISO = Pattern.compile("^\\d{4}[\\/-]\\d{1,2}[\\/-]\\d{1,2}$");
+    /**
+     * 正则表达式Pattern： number
+     */
+    public static final Pattern pattern_number = Pattern.compile("^-?(?:\\d+|\\d{1,3}(?:,\\d{3})+)(?:\\.\\d+)?$");
+    /**
+     * 正则表达式Pattern： numberDE
+     */
+    public static final Pattern pattern_number_DE = Pattern.compile("^-?(?:\\d+|\\d{1,3}(?:\\.\\d{3})+)(?:,\\d+)?$");
+    /**
+     * 正则表达式Pattern： digits 整数
+     */
+    public static final Pattern pattern_digits = Pattern.compile("^\\d+$");
+    /**
+     * 正则表达式Pattern： letters with basic punc
+     */
+    public static final Pattern pattern_letters_with_basic_punc = Pattern.compile("^[a-z-.,()'\"\\s]+$", Pattern.CASE_INSENSITIVE);
+    /**
+     * 正则表达式Pattern： alpha numeric,只能包含字母、数字、空格或下划线
+     */
+    public static final Pattern pattern_alpha_numeric = Pattern.compile("^\\w+$", Pattern.CASE_INSENSITIVE);
+    /**
+     * 正则表达式Pattern： letters only,只能包含字母
+     */
+    public static final Pattern pattern_letters_only = Pattern.compile("^[a-z]+$", Pattern.CASE_INSENSITIVE);
+    /**
+     * 正则表达式Pattern： letters only,只能包含大写字母
+     */
+    public static final Pattern pattern_Uppercase_only = Pattern.compile("^[A-Z]+$");
+    /**
+     * 正则表达式Pattern： letters only,只能包含小写字母
+     */
+    public static final Pattern pattern_Lowercase_only = Pattern.compile("^[a-z]+$");
+    /**
+     * 正则表达式Pattern： no whitespace 不能包含非打印字符
+     */
+    public static final Pattern pattern_no_whitespace = Pattern.compile("^\\S+$");
+    /**
+     * 正则表达式Pattern： zip code
+     */
+    public static final Pattern pattern_zip_code = Pattern.compile("^\\d{6}$");
+    /**
+     * 正则表达式Pattern： id card 身份证号码
+     */
+    public static final Pattern pattern_id_card = Pattern.compile("^[1-9]\\d{13}[\\dx]|[1-9]\\d{16}[\\dx]$", Pattern.CASE_INSENSITIVE);
+    /**
+     * 正则表达式Pattern： time
+     */
+    public static final Pattern pattern_time = Pattern.compile("^([01][0-9])|(2[0123]):([0-5])([0-9])$");
+    /**
+     * 正则表达式Pattern： phone
+     */
+    public static final Pattern pattern_phone = Pattern.compile("^(0?\\d{2,3}-?)?\\d{7,8}(-\\d{3,})?$");
+    /**
+     * 正则表达式Pattern： phone2 电话号码的函数(包括验证国内区号,国际区号,分机号)
+     */
+    public static final Pattern pattern_phone2 = Pattern.compile("^(([0\\+]\\d{2,3}-)?(0\\d{2,3})-)?(\\d{7,8})(-(\\d{3,}))?$");
+    /**
+     * 正则表达式Pattern： 手机号码
+     */
+    public static final Pattern pattern_mobile = Pattern.compile("^0?1[3578]\\d{9}$");
+    /**
+     * 正则表达式Pattern： Color
+     */
+    public static final Pattern pattern_colr = Pattern.compile("^#[a-f0-9]{6}$", Pattern.CASE_INSENSITIVE);
+    /**
+     * 正则表达式Pattern： ip 4
+     */
+    public static final Pattern pattern_ip4 = Pattern.compile("^(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)$");
+}
+
