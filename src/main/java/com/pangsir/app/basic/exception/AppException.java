cbb8c15fe27f72a1429198faf7b7c942e512b02b
pangsir8983 <pangsir8983@gmail.com>
2016-05-30 16:18:20 +0800

    偷偷的扩展了Exception,但是有点感觉基础薄弱!

--- src/main/java/com/pangsir/app/basic/exception/AppException.java
+++ src/main/java/com/pangsir/app/basic/exception/AppException.java
@@ -1,24 +1,65 @@
 package com.pangsir.app.basic.exception;
 
+import java.util.Collection;
+import java.util.Vector;
+
 /**
- * 类说明: 整个项目异常的基础实现
+ * 类说明: 扩展Exception增加属性和异常栈
+ * --- 模拟宋立珍提供的部分代码
  *
  * @Author: 胖先生
- * @Create: 2016-05-12 11:35
+ * @Create: 2016-05-30 14:09
  * @Home: http://www.cnblogs.com/pangxiansheng/
  */
 
-public class AppException extends RuntimeException {
+public class AppException extends  java.lang.Exception {
 
-    public AppException() {
-        super();
+    private Vector vectorErrorMsg;
+
+    public AppException(String errorMsg){
+        vectorErrorMsg = new Vector(2);
+        vectorErrorMsg.add(errorMsg);
     }
 
-    public AppException(String message) {
-        super(message);
+    public AppException(String errorMsg,AppException ae){
+        this(errorMsg);
+        setStackTrace(ae.getStackTrace());
+        vectorErrorMsg.addAll(getErrorList());
     }
 
-    public AppException(String message, Throwable cause) {
-        super(message, cause);
+    private Collection getErrorList(){
+        return vectorErrorMsg;
     }
+
+    public AppException(String errorMsg,Exception ex){
+        this(errorMsg);
+        setStackTrace(ex.getStackTrace());
+        //获取错误的信息
+        String message = ex.getMessage();
+        /* 定制
+        if(null!=message&&(message.length()==7||message.length()>7&&' '==message.charAt(7))){
+            vectorErrorMsg.add(message);
+        }*/
+        if (null!=message&&message.trim().length()>0){
+            vectorErrorMsg.add(message);
+        }
+    }
+    @Override
+    public String getMessage(){
+        return (String)vectorErrorMsg.firstElement();
+    }
+
+    public String getAllMessage(){
+        StringBuilder result = new StringBuilder();
+        for(int i=0,length = vectorErrorMsg.size();i<length;i++){
+            result.append(String.valueOf(vectorErrorMsg.get(i)));
+            if(i!=length-1){
+                result.append("\r\n");
+            }
+        }
+
+        return result.toString();
+
+    }
+
 }
