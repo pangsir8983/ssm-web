4df9c938685c55b41e9caa4669431e15f9292b8e
pangsir8983 <pangsir8983@gmail.com>
2016-05-30 16:25:52 +0800

    一些 备份文件!

---
+++ .idea/fileTemplates/internal/Class.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#set($USER_NAME="胖先生")
+#set($URL="http://www.cnblogs.com/pangxiansheng/")
+/**
+ * 类说明: ${DESCRIPTION}
+ * @Author: ${USER_NAME}
+ * @Create: ${YEAR}-${MONTH}-${DAY} ${TIME}
+ * @Home: ${URL}
+ */
+
+public class ${NAME} {
+}
